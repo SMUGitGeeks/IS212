b9594ec6f2610617b4c8890bc33dda7ce8452d5f
DarrylSSY <ssy.darryl@gmail.com>
2023-09-16 00:13:03 +0800

    [SBRP-32] Update ExpressJS to run on port 5000
    Modify ExpressJS configuration to use port 5000 for the server, ensuring that port 3000 remains reserved for the frontend.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Load libraries and variables
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Load routes
 app.get('/', (req, res) => { res.send('API Running'); });
