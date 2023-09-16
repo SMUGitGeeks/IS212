720876b6c9f741014fda3243c4bc93e231a55d81
DarrylSSY <ssy.darryl@gmail.com>
2023-09-16 19:52:19 +0800

    [SBRP-30] Integrate MySQL database into backend
    Integrate MySQL database functionality into the ExpressJS backend to store and manage application data.

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 // Load libraries and variables
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 // Load routes
 app.get('/', (req, res) => { res.send('API Running'); });
