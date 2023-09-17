2fa661b3e90108247e1c0a3883ed7bc6daef9288
DarrylSSY <ssy.darryl@gmail.com>
2023-09-18 01:11:04 +0800

    [SBRP-33] Setup base pages for future purposes
    Add navbar with router functionalities and removed redundant files. Add Bootstrap container.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Button } from 'antd';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Roles from './components/pages/Roles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button type="primary">Button</Button>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Fragment>
+            <Navbar />
+            {/*Create route for Home*/}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/roles" element={<Roles />} />
+            </Routes>
+        </Fragment>
+      </Router>
   );
 }
 
---
+++ client/src/components/pages/Roles.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import type { MenuProps } from 'antd';
+import {Container} from "react-bootstrap";
+
+function Roles() {
+    return (
+        <Container>
+            Skill Based Role Portal - Roles
+        </Container>
+    )
+}
+
+export default Roles;
