1125a5c63aa3e4847bc9e07f827669232fd8dc05
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-09-20 15:52:29 +0800

    [SBRP-13] Clean up SearchBar

--- client/src/components/layout/SearchBar.tsx
+++ client/src/components/layout/SearchBar.tsx
@@ -3,15 +3,17 @@ import { Input} from 'antd';
 import { SearchProps } from 'antd/es/input';
 
 const { Search } = Input;
-const suffix = (
-    <AudioOutlined
-      style={{
-        fontSize: 16,
-        color: '#1677ff',
-      }}
-    />
-  );
-const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+// const suffix = (
+//     <AudioOutlined
+//       style={{
+//         fontSize: 16,
+//         color: '#1677ff',
+//       }}
+//     />
+//   );
+const onSearch: SearchProps['onSearch'] = (value, _e, info) => {
+    console.log(value)
+};
 // const onSearch = (value: String, _e: any) => console.log(value);
 
 function SearchBar() {
