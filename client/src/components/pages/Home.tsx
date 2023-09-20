3bf133f9f4d7c02c70f613a44c9561fc953e347f
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-09-20 09:37:36 +0800

    [SBRP-37] Add UI Element Placeholders

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import type { MenuProps } from 'antd';
 import {Container} from "react-bootstrap";
+import JobList from '../layout/JobList';
+import { AudioOutlined } from '@ant-design/icons';
+import {Row, Col, Input, Space} from 'antd';
+
+const { Search } = Input;
+const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: '#1677ff',
+      }}
+    />
+  );
+// const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+const onSearch = (value: String, _e: any) => console.log(value);
 
 function Home() {
     return (
         <Container>
-            Skill Based Role Portal - Home
+            {/* Skill Based Role Portal - Home */}
+            <Space direction="vertical" size={48} style={{ display: 'flex' }}>
+                <div></div>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
+                    <Col span={22}>
+                        <Search placeholder="Search Roles" onSearch={onSearch} />
+                    </Col>
+                </Row>
+                
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
+                    <Col span={5}>
+                        Filter
+                    </Col>
+                    <Col span={17}>
+                        <JobList />
+                    </Col>
+                </Row>
+            </Space>
         </Container>
     )
 }
--- client/src/components/pages/Roles.tsx
+++ client/src/components/pages/Roles.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import type { MenuProps } from 'antd';
+// import type { MenuProps } from 'antd';
 import {Container} from "react-bootstrap";
+import ApplicationBar from '../layout/ApplicationBar';
+
+
 
 function Roles() {
     return (
         <Container>
+            <ApplicationBar />
             Skill Based Role Portal - Roles
+
+            
+            
         </Container>
     )
 }
