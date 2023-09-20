ac39ade8ff9341fbc92eb238ff2e172bf8a4e953
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-09-20 15:18:26 +0800

    [SBRP-13] Move Rolelist view to Roles page

--- client/src/components/pages/Roles.tsx
+++ client/src/components/pages/Roles.tsx
@@ -3,7 +3,9 @@ import {Container} from "react-bootstrap";
 import { connect } from 'react-redux';
 import { getRoles } from '../../actions/role';
 import PropTypes from 'prop-types';
-import ApplicationBar from '../layout/ApplicationBar';
+import RoleList from '../layout/RoleList';
+import {Row, Col, Space} from 'antd';
+import SearchBar from '../layout/SearchBar';
 
 
 const Roles = ({ getRoles, role: { roles, loading } }: any) => {
@@ -17,7 +19,25 @@ const Roles = ({ getRoles, role: { roles, loading } }: any) => {
             {roles.map((role: any)=> (
                 <div> {role.rl_desc} </div>
                 ))}
-            <ApplicationBar />
+
+            {/* Design section */}
+            <Space direction="vertical" size={48} style={{ display: 'flex' }}>
+                <div></div>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
+                    <Col span={22}>
+                        <SearchBar />
+                    </Col>
+                </Row>
+                
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
+                    <Col span={5}>
+                        Filter
+                    </Col>
+                    <Col span={17}>
+                        <RoleList />
+                    </Col>
+                </Row>
+            </Space>
         </Container>
 
 }
