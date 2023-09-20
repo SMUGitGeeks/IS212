0ea3dd805250bcca54aab4109b408a36ae85b8d8
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-09-20 10:41:15 +0800

    [SBRP-37] Add SearchBar component

---
+++ client/src/components/layout/SearchBar.tsx
@@ -0,0 +1,23 @@
+import { AudioOutlined } from '@ant-design/icons';
+import { Input} from 'antd';
+import { SearchProps } from 'antd/es/input';
+
+const { Search } = Input;
+const suffix = (
+    <AudioOutlined
+      style={{
+        fontSize: 16,
+        color: '#1677ff',
+      }}
+    />
+  );
+const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
+// const onSearch = (value: String, _e: any) => console.log(value);
+
+function SearchBar() {
+    return (
+        <Search placeholder="Search Roles" onSearch={onSearch} />
+    )
+}
+
+export default SearchBar;
--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -2,21 +2,10 @@ import React from 'react';
 import type { MenuProps } from 'antd';
 import {Container} from "react-bootstrap";
 import JobList from '../layout/JobList';
-import { AudioOutlined } from '@ant-design/icons';
-import {Row, Col, Input, Space} from 'antd';
-import { SearchProps } from 'antd/es/input';
+import {Row, Col, Space} from 'antd';
+import SearchBar from '../layout/SearchBar';
 
-const { Search } = Input;
-const suffix = (
-    <AudioOutlined
-      style={{
-        fontSize: 16,
-        color: '#1677ff',
-      }}
-    />
-  );
-const onSearch: SearchProps['onSearch'] = (value, _e, info) => console.log(info?.source, value);
-// const onSearch = (value: String, _e: any) => console.log(value);
+
 
 function Home() {
     return (
@@ -26,7 +15,7 @@ function Home() {
                 <div></div>
                 <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} justify={'center'}>
                     <Col span={22}>
-                        <Search placeholder="Search Roles" onSearch={onSearch} />
+                        <SearchBar />
                     </Col>
                 </Row>
                 
