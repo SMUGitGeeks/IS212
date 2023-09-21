f298f262b62fa00719eb272952c44cfe2c6afd94
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-09-21 22:18:22 +0800

    [SBRP-37] Remove unused routing in Home

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import {Container} from "react-bootstrap";
 import ApplicationBar from '../layout/ApplicationBar';
-import { Routes, Route, useLocation, Router } from 'react-router-dom';
-
 
 function Home() {
-    const location = useLocation();
-    console.log(location)
     return (
         <Container>
             <ApplicationBar />
