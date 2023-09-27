14f688488ec0e0d6c466c71cfb985a9800ca8e81
Darryl Soh <ssy.darryl@gmail.com>
2023-09-27 14:53:15 +0800

    [SBRP-58] Create login page
    Setup login page using redux functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,27 @@
-import React, { Fragment } from 'react';
+import React, {Fragment, useEffect} from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Roles from './components/pages/Roles';
-import { Provider } from 'react-redux';
-import store from './store';
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
+import {loginHR, loginStaff} from "./actions/auth";
+import Login from "./components/pages/Login";
 
 
-function App() {
+
+const App = ({auth: {user, loading}}:any ) => {
   return (
-      <Provider store={store}>
+      user=="" ?
+          <Fragment>
+            <Login />
+          </Fragment> :
+
         <Router>
             <Fragment>
                 <Navbar />
+                <div>Currently logged in as {user}</div>
                 {/*Create route for Home*/}
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -21,8 +29,17 @@ function App() {
                 </Routes>
             </Fragment>
           </Router>
-      </Provider>
   );
 }
 
-export default App;
+
+App.propTypes = {
+    loginStaff: PropTypes.func.isRequired,
+    loginHR: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state: any) => ({
+    auth: state.auth
+});
+export default connect(mapStateToProps, { loginStaff, loginHR })(App);
