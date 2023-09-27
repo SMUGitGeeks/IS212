42e20ec307e8fa26d167b5331873b094e78eed95
DarrylSSY <ssy.darryl@gmail.com>
2023-09-28 00:07:13 +0800

    Create types for roleListings.tsx
    Create types for roleListings.tsx

---
+++ client/src/types.tsx
@@ -0,0 +1,17 @@
+export interface RoleListingsType {
+    rl_close: string;
+    rl_creator: number;
+    rl_desc: string;
+    rl_id: number;
+    rl_open: string;
+    rl_ts_create: string;
+    role_description: string;
+    role_id: number;
+    role_name: string;
+    role_status: string;
+}
+
+export interface ActionType {
+    type: any;
+    payload: any;
+}
