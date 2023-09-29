d2256cac215a682b9ce8036b11ab006b97ff6cc1
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-09-29 15:29:19 +0800

    [SBRP-48] Fix RolePage UI

--- client/src/components/layout/RoleDescription.tsx
+++ client/src/components/layout/RoleDescription.tsx
@@ -7,11 +7,11 @@ import {
     Row,
     Space,
     Tag,
-    Tooltip,
     Typography,
+    Progress,
 } from "antd";
 import { Container } from "react-bootstrap";
-import { StarOutlined } from "@ant-design/icons";
+import { SolutionOutlined, AimOutlined, CheckCircleOutlined, ClockCircleOutlined } from "@ant-design/icons";
 import { rowGutterStyle } from "../../App";
 import React, { useEffect } from "react";
 import { getRoleSkillsByRoleId } from "../../actions/roleSkills";
@@ -20,7 +20,27 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { useParams } from "react-router-dom";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
+
+const color = (status: String) => {
+    if (status === "active") {
+        return "green";
+    } else if (status === "in-progress") {
+        return "gold";
+    } else {
+        return "default";
+    }
+}
+
+const tagIcon = (status: String) => {
+    if (status === "active") {
+        return <CheckCircleOutlined />;
+    } else if (status === "in-progress") {
+        return <ClockCircleOutlined />;
+    } else {
+        return <></>;
+    }
+}
 
 export const RoleDescription = ({
     getRoleListing,
@@ -29,6 +49,7 @@ export const RoleDescription = ({
     getRoleSkillsByRoleId,
     staffSkills: { staffSkill },
 }: any) => {
+    
     const { roleListingId } = useParams();
     useEffect(() => {
         getRoleListing(roleListingId);
@@ -37,52 +58,35 @@ export const RoleDescription = ({
 
     const calculateSkillsMatch = () => {
         let matchedSkills = 0;
-        let missingSkills = 0;
-        // missingSkillNames is an array of strings
+        // let missingSkills = 0;
+        let numRoleSkills = roleSkills.length;
+        
         let missingSkillNames = [] as any;
-        let match = 0;
         staffSkill.forEach((staffSkill: any) => {
             roleSkills.forEach((roleSkill: any) => {
                 if (
                     staffSkill.skill_id === roleSkill.skill_id &&
-                    staffSkill.skill_status === "active"
+                    (staffSkill.skill_status === "in-progress" || staffSkill.skill_status === "unverified")
                 ) {
-                    matchedSkills += 1;
+                    missingSkillNames.push([roleSkill.skill_name, roleSkill.skill_status]);
+                } else if (
+                    staffSkill.skill_id !== roleSkill.skill_id
+                ) {
+                    missingSkillNames.push([roleSkill.skill_name, "missing"]);
                 } else {
-                    missingSkillNames.push(roleSkill.skill_name);
-                    missingSkills += 1;
+                    matchedSkills++;
                 }
             });
         });
 
-        match = (matchedSkills / (matchedSkills + missingSkills)) * 100;
+        let match = (matchedSkills / numRoleSkills) * 100;
         return [match.toFixed(2), missingSkillNames];
     };
 
-    // Skills Matching ===================
-    // const missingSkills: String[] = [
-    //     "Fishing",
-    //     "cars"
-    // ];
-    // const matchedSkills: String[] = [
-    //     "Lorry"
-    // ]
-    // const items: CollapseProps['items'] = [
-    //     {
-    //         key: '1',
-    //         label: 'Skills Matched',
-    //         children: (matchedSkills.map((skill: any) => (
-    //             <Tag>{skill}</Tag>
-    //         ))),
-    //     },
-    //     {
-    //         key: '2',
-    //         label: 'Skills Missing',
-    //         children: (missingSkills.map((skill: any) => (
-    //             <Tag>{skill}</Tag>
-    //         ))),
-    //     },
-    // ];
+    const [matchPercentage, missingSkills] = calculateSkillsMatch();
+
+    // const calculateDateDifference = () => {};
+    
     const onChange = (key: string | string[]) => {
         // console.log(key);
     };
@@ -109,50 +113,68 @@ export const RoleDescription = ({
         <Container>
             <Space direction="vertical" style={{ display: "flex" }} size="large">
                 <Row gutter={rowGutterStyle} align="middle">
-                    <Col span={20}>
+                    <Col xs={24} sm={24} md={15} lg={17} xl={19}>
                         <div className="">
                             <h1>{roleListing.role_name}</h1>
-                            <h3>Department</h3>
+                            <h3>"Department"</h3>
+                            <Space direction="horizontal" wrap>
+                                
+                            </Space>
                         </div>
                     </Col>
-                    <Col span={4}>
+                    <Col xs={24} sm={24} md={9} lg={7} xl={5}>
                         <Space direction="vertical" align="center" size="large">
-                            <Tooltip title="Add to Favourites">
-                                <StarOutlined style={{ fontSize: 24 }} />
-                            </Tooltip>
-
-                            <Button type="primary">Apply Now</Button>
+                            <Button type="primary" size="large" icon={<SolutionOutlined />}>Apply Now</Button>
                         </Space>
                     </Col>
                 </Row>
 
-                <Row gutter={rowGutterStyle} justify="center">
-                    <Col span={18}>{roleListing.role_description}</Col>
-                    <Col span={6}>
+                <Row gutter={rowGutterStyle} justify="space-between">
+                    {/* Description coloumn */}
+                    <Col xs={24} sm={24} md={24} lg={15} xl={17}>
                         <Space direction="vertical">
-                            <Descriptions
-                                title="User Info"
-                                layout="vertical"
-                                items={importantDetails}
-                            />
-                            <Divider />
+                            {
+                                roleListing.rl_desc ? 
+                                <>
+                                    <Divider orientation="left" orientationMargin="0" style={{fontSize: 23}}>Details</Divider>
+                                    {roleListing.rl_desc}
+                                </> : <></>
+                            }
+                            <p style={{padding: "2%"}}></p>
+                            {
+                                roleListing.role_description ? 
+                                <>
+                                    <Divider orientation="left" orientationMargin="0" style={{fontSize: 23}}>Role Description</Divider>
+                                    {roleListing.role_description}
+                                </> : <></>
+                            }
+                        </Space>
+                    </Col>
+
+                    <Col xs={24} sm={24} md={24} lg={0} style={{paddingTop: "10%"}}><Divider /></Col>
+
+                    {/* Skills Column */}
+                    <Col xs={24} sm={24} md={24} lg={8} xl={6} xxl={5}>
+                        <Space direction="vertical" style={{width: "100%"}}>
+                            <Space direction="horizontal" align="center">
+                                <AimOutlined style={{fontSize: 25}}/>
+                                <p style={{fontSize: 26, margin: "0"}}>{matchPercentage}%</p>
+                                <p style={{fontSize: 12, color: "grey", margin: "0"}}>Skills<br />Matched</p>
+                            </Space>
+                            <Progress percent={matchPercentage} showInfo={false} />
+
                             {/* Display Role's skills */}
-                            <Title level={5}>All Skills Required</Title>
+                            <Divider orientation="left" orientationMargin="0">All Skills Required</Divider>
                             <Space size={[0, 8]} wrap>
                                 {roleSkills.map((skill: any) => (
                                     <Tag>{skill.skill_name}</Tag>
                                 ))}
                             </Space>
 
-                            <Title level={5}>Skills Match</Title>
+                            <Title level={5}>Missing Skills</Title>
                             <Space size={[0, 8]} wrap>
-                                {calculateSkillsMatch()[0]}%
-                            </Space>
-
-                            <Title level={5}>Skills Missing</Title>
-                            <Space size={[0, 8]} wrap>
-                                {calculateSkillsMatch()[1].map((skill: any) => (
-                                    <Tag>{skill}</Tag>
+                                {missingSkills.map((skill: any) => (
+                                    <Tag icon={tagIcon(skill.skill_status)} color={color(skill.skill_status)}>{skill[0]}</Tag>
                                 ))}
                             </Space>
 
@@ -160,12 +182,6 @@ export const RoleDescription = ({
                             {/* <Collapse items={items} defaultActiveKey={['1']} onChange={onChange} /> */}
                         </Space>
                     </Col>
-                </Row>
-                <Row gutter={rowGutterStyle} justify="center">
-                    <img 
-                     style={{ width: '100%', height: '240px' }}
-                    src="https://media.licdn.com/dms/image/C4E03AQEC1MBLS705zA/profile-displayphoto-shrink_400_400/0/1650103443401?e=1701302400&v=beta&t=01qUXlg6azTUgYa_q9-NxGc8nzvLmxeAVvO3XIK4TMY"/>
-                    hi Glen here, I am a row GAHAHAHAHA
                 </Row>
             </Space>
         </Container>
