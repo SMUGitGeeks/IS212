0250c79878669fa17260ba673935e48d42137ef8
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-10-21 14:20:43 +0800

    [SBRP] TDD Test for loading element in HrRoleListings
    And Clean up other test cases to incorporate async loading

--- client/src/components/layout/HrRoleListings.test.tsx
+++ client/src/components/layout/HrRoleListings.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor, waitForElementToBeRemoved} from '@testing-library/react';
 import {store} from '../../mockStore';
 import {Provider} from 'react-redux';
 import {mockMatchMedia} from "../../setupTests";
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom'
 import HrRoleListings from './HrRoleListings';
 import userEvent from '@testing-library/user-event';
 
-describe('Role List component tests', () => {
+describe('Hr Role Listings component tests', () => {
     beforeAll(() => {
         mockMatchMedia();
     });
@@ -18,9 +18,8 @@ describe('Role List component tests', () => {
                 </BrowserRouter>
             </Provider>)
     });
-    
 
-    it ("able to click All radio button",() => {
+    it ("able to click All radio button", () => {
         userEvent.click(screen.getByText('All'))
         expect(screen.getByLabelText('All')).toBeChecked()
     });
@@ -40,55 +39,81 @@ describe('Role List component tests', () => {
         expect(screen.getByLabelText('Open')).toBeChecked()
     });
 
-    it ("able to click edit icon and redirect to update page",() => {
-        const editIconElement = screen.getAllByTestId('edit-icon-click')[0];
+    it('should load 3 seconds before displaying data', async () => {
+        expect(screen.getByTestId("skeleton-list")).toBeInTheDocument();
 
-        userEvent.click(editIconElement)
-        expect(window.location.pathname).toBe('/listingManage/update/1');
+        waitFor(() => {
+            expect( screen.queryByTestId("skeleton-list")).not.toBeInTheDocument();
+            expect( screen.getByText('HR Manager')).toBeInTheDocument();
+            expect( screen.getByText('IT Technician')).toBeInTheDocument();
+        });
+    });
+    
+    it ("able to click edit icon and redirect to update page", async () => {
+        waitFor(() => {
+            const editIconElement = screen.getAllByTestId('edit-icon-click')[0];
+            expect(editIconElement).toBeInTheDocument();
+
+            userEvent.click(editIconElement)
+            expect( window.location.pathname).toBe('/listingManage/update/1');
+        });
     });
 
-    it ("able to click edit icon and redirect to listing page",() => {
+    it ("able to click edit icon and redirect to listing page", async () => {
+    waitFor(() => {
         const listItemElement = screen.getAllByTestId('one-listing')[0];
 
         userEvent.click(listItemElement)
         expect(window.location.pathname).toBe('/listingManage/1');
     });
+    });
 
-    it ("should have 5 role listings in total",() => {
+    it ("should have 5 role listings in total", async () => {
+    waitFor(() => {
         const listItemElement = screen.getAllByTestId('one-listing');
 
         expect(listItemElement.length).toBe(5);
     });
+    });
 
-    it ("should have 0 scheduled role listings in total",() => {
+    it ("should have 0 scheduled role listings in total", async () => {
+    waitFor(() => {
         userEvent.click(screen.getByText('Scheduled'))
         const listItemElement = screen.queryAllByTestId('one-listing');
 
         expect(listItemElement.length).toBe(0);
     });
+    });
 
-    it ("should have 3 open role listings",() => {
+    it ("should have 3 open role listings", async () => {
+    waitFor(() => {
         userEvent.click(screen.getAllByText('Open')[0])
         const listItemElement = screen.getAllByTestId('one-listing');
 
         expect(listItemElement.length).toBe(3);
     });
+    });
 
-    it ("should have 2 closed role listings",() => {
+    it ("should have 2 closed role listings", async () => {
+    waitFor(() => {
         userEvent.click(screen.getAllByText('Closed')[0])
         const listItemElement = screen.getAllByTestId('one-listing');
 
         expect(listItemElement.length).toBe(2);
         console.log(window.location.pathname)
     });
+    });
 
-    it ("should display listing statuses",() => {
+    it ("should display listing statuses", async () => {
+    waitFor(() => {
         const listItemElement = screen.getAllByTestId('status');
 
         expect(listItemElement.length).toBe(5);
     });
+    });
 
-    it ("HR Manager should display open status", () => {
+    it ("HR Manager should display open status", async () => {
+    waitFor(() => {
         const statusElement = screen.getAllByTestId('status')[0];
         const hrManagerElement = screen.getByText("HR Manager");
         const engineeringManagerElement = screen.getAllByText("Engineering Manager")[0];
@@ -100,8 +125,10 @@ describe('Role List component tests', () => {
         expect(hrManagerElement.compareDocumentPosition(statusElement)).toBe(4);
         expect(statusElement.compareDocumentPosition(engineeringManagerElement)).toBe(4);
     });
+    });
 
-    it ("HR Manager should display creator as John Doe on 01/08/2023", () => {
+    it ("HR Manager should display creator as John Doe on 01/08/2023", async () => {
+    waitFor(() => {
         const creatorElement = screen.getAllByText('Creator:')[0];
         const creatorDetailsElement = screen.getByText('John Doe | 01/08/2023');
         const updatorElement = screen.getAllByText('Last Updator:')[0];
@@ -118,8 +145,10 @@ describe('Role List component tests', () => {
         expect(creatorElement.compareDocumentPosition(updatorElement)).toBe(4);
         expect(creatorElement.compareDocumentPosition(engineeringManagerElement)).toBe(4);
     });
+    });
 
-    it ("HR Manager should display updator as John Doe on 13//10/2023", () => {
+    it ("HR Manager should display updator as John Doe on 13//10/2023", async () => {
+    waitFor(() => {
         const updatorElement = screen.getAllByText('Last Updator:')[0];
         const updatorDetailsElement = screen.queryAllByText('John Doe | 13/10/2023')[0];
         const hrManagerElement = screen.getByText("HR Manager");
@@ -135,9 +164,11 @@ describe('Role List component tests', () => {
         expect(updatorDetailsElement.compareDocumentPosition(updatorElement)).toBe(2);
         expect(updatorDetailsElement.compareDocumentPosition(engineeringManagerElement)).toBe(4);
     });
+    });
 
-    it ("Engineer Manager should display closed status", () => {
-        screen.debug(undefined, Infinity);
+    it ("Engineer Manager should display closed status", async () => {
+    waitFor(() => {
+        // screen.debug(undefined, Infinity);
         const statusElement = screen.getAllByTestId('status')[3];
         const itTechnicianElement = screen.getByText("IT Technician");
         const financeStaffElement = screen.getByText("Finance Staff");
@@ -149,8 +180,10 @@ describe('Role List component tests', () => {
         expect(itTechnicianElement.compareDocumentPosition(statusElement)).toBe(4);
         expect(statusElement.compareDocumentPosition(financeStaffElement)).toBe(4);
     });
+    });
 
-    it ("Engineer Manager should display creator as John Doe on 01/08/2023", () => {
+    it ("Engineer Manager should display creator as John Doe on 01/08/2023", async () => {
+    waitFor(() => {
         const creatorElement = screen.getAllByText('Creator:')[3];
         const creatorDetailsElement = screen.getByText('John Doe | 14/08/2023');
         const itTechnicianElement = screen.getByText("IT Technician");
@@ -164,15 +197,19 @@ describe('Role List component tests', () => {
         expect(itTechnicianElement.compareDocumentPosition(creatorElement)).toBe(4);
         expect(creatorElement.compareDocumentPosition(financeStaffElement)).toBe(4);
     });
+    });
 
-    it ("Engineer Manager should not display an Updator", () => {
+    it ("Engineer Manager should not display an Updator", async () => {
+    waitFor(() => {
         const engineeringManagerListElement = screen.getAllByTestId('one-listing')[3];
 
         expect(engineeringManagerListElement).toBeInTheDocument();
         expect(engineeringManagerListElement).not.toHaveTextContent('Last Updator:');
     });
+    });
 
-    it ("should display location as Thailand for HR Manager", () => {
+    it ("should display location as Thailand for HR Manager", async () => {
+    waitFor(() => {
         const locationElement = screen.getAllByText('Thailand')[0];
         const hrManagerElement = screen.getByText("HR Manager");
         const engineeringManagerElement = screen.getAllByText("Engineering Manager")[0];
@@ -184,8 +221,10 @@ describe('Role List component tests', () => {
         expect(hrManagerElement.compareDocumentPosition(locationElement)).toBe(4);
         expect(locationElement.compareDocumentPosition(engineeringManagerElement)).toBe(4);
     });
+    });
 
-    it ("should display Human Resources as department for HR Manager", () => {
+    it ("should display Human Resources as department for HR Manager", async () => {
+    waitFor(() => {
         const departmentElement = screen.getAllByText('Human Resources')[0];
         const hrManagerElement = screen.getByText("HR Manager");
         const engineeringManagerElement = screen.getAllByText("Engineering Manager")[0];
@@ -196,5 +235,6 @@ describe('Role List component tests', () => {
         
         expect(hrManagerElement.compareDocumentPosition(departmentElement)).toBe(4);
         expect(departmentElement.compareDocumentPosition(engineeringManagerElement)).toBe(4);
+    });
     });
 })
