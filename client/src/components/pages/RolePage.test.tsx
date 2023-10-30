88161393fb19c285af85d0f688d786f8e01a68bc
Seah Yok Sim <yoksim.seah.2021@scis.smu.edu.sg>
2023-10-30 17:20:52 +0800

    [SBRP-101] Fix affected test script from new changes

--- client/src/components/pages/RolePage.test.tsx
+++ client/src/components/pages/RolePage.test.tsx
@@ -1,9 +1,11 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, waitFor} from '@testing-library/react';
 import RolePage from './RolePage';
 import {store} from '../../mockStore';
 import {Provider} from 'react-redux';
 import {mockMatchMedia} from "../../setupTests";
 import {MemoryRouter} from "react-router-dom";
+
+jest.useFakeTimers();
 
 describe('RolePage component tests', () => {
     beforeAll(() => {
@@ -17,23 +19,29 @@ describe('RolePage component tests', () => {
                 </MemoryRouter>
             </Provider>);
     });
-    it('Should see the Apply Now button', () => {
-        const applyNowElement = screen.getByText("Apply Now");
-        expect(applyNowElement).toBeInTheDocument();
+    it('Should see the Apply Now button', async () => {
+        jest.advanceTimersByTime(2000);
+        await waitFor(() => {
+            const applyNowElement = screen.getByText("Apply Now");
+            expect(applyNowElement).toBeInTheDocument();
+        });
     })
-    it('Should see All Skills', () => {
-        const allSkillsElement = screen.getByText("All Skills Required");
-        const communicationSkillElement = screen.getByText("Communication");
-        const teamworkSkillElement = screen.getByText("HR management");
-        expect(allSkillsElement).toBeInTheDocument();
-        expect(communicationSkillElement).toBeInTheDocument();
-        expect(teamworkSkillElement).toBeInTheDocument();
+    it('Should see All Skills', async () => {
+        jest.advanceTimersByTime(2000);
+        await waitFor(() => {
+            const allSkillsElement = screen.getByText("All Skills Required");
+            const communicationSkillElement = screen.getByText("Communication");
+            const teamworkSkillElement = screen.getByText("HR management");
+            expect(allSkillsElement).toBeInTheDocument();
+            expect(communicationSkillElement).toBeInTheDocument();
+            expect(teamworkSkillElement).toBeInTheDocument();
+        });
     })
-    it('Should show HR Manager', () => {
-        const hrManagerElement = screen.getByText("HR Manager");
-        expect(hrManagerElement).toBeInTheDocument();
+    it('Should show HR Manager', async () => {
+        jest.advanceTimersByTime(2000);
+        await waitFor(() => {
+            const hrManagerElement = screen.getByText("HR Manager");
+            expect(hrManagerElement).toBeInTheDocument();
+        });
     })
-
 })
-
-export {}
